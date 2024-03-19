eff6dcbc8eb8cea293cccc253927659e2436028a
Husky Lover <ssolivares0@gmail.com>
2024-03-18 23:18:15 -0400

    implemented partial code for project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-#weather-dashboard
+Module 6- Weather Dashboard Challenge
+The purpose of this challenge was to create a weather dashboard using the OpenWeather API. 
+I used these webpages as reference on how to make appropriate API calls. 
+[How To Use Api Keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys)
+[Free Code Camp](https://www.freecodecamp.org/news/javascript-insertadjacenthtml-method-efficient-dom-manipulation/#:~:text=It's%20also%20the%20go%2Dto,DOM%20(Document%20Object%20Model).)
+[Free Code Camp](https://www.freecodecamp.org/news/make-api-calls-in-javascript/)
+[deployed webpage](https://sandyvzqz.github.io/weather-dashboard/)
+Screenshot of Webpage:
+[webpage screenshot](./sandyvzqz.github.io_weather-dashboard_.png)
--- develop/js/script.js
+++ develop/js/script.js
@@ -1,26 +1,47 @@
 // declare useful variables for future use
 const searchButton = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
-const apiKey = 'fddf8f1a953a537d5becaaab06aa72a8'
+const apiKey = 'fddf8f1a953a537d5becaaab06aa72a8';
+const weatherCards = document.querySelector(".weather-cards");
+const currentWeather= document.querySelector(".current-weather");
+
+// gets information from array and inputs it into weather cards
+const createWeatherCard= (weatherItem) => {
+    return `<li class="card">
+                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                <img src="<img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather icon">
+                <h4>Temperature: ${weatherItem.main.temp}&degF</h4>
+                <h4>Wind: ${weatherItem.wind.speed}MPH</h4>
+                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+            </li>`;
+}
 
 const getWeatherDetails= (cityName, lat, lon) => {
     const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=`+lat+`&lon=`+lon+`&appid=`+apiKey;
     fetch(weatherApiUrl).then(res => res.json()).then(data => {
-        console.log(data); //check to see if code is working
+        console.log(data); //check to see if code is working in console
         //set the forecast to one forecast per day
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
             const forecastDate = new Date(forecast.dt_txt).getDate();
-            if(uniqueForecastDays.includes(forecastDate)){
+            if(!uniqueForecastDays.includes(forecastDate)){
                 return uniqueForecastDays.push(forecastDate);
             }
         });
+        //clears both inputs
+        cityInput.value = "";
+        weatherCards.innerHTML = "";
+
         console.log(fiveDaysForecast);
+        fiveDaysForecast.forEach(weatherItem => {
+            // createWeatherCard(weatherItem);
+            //used FreeCodeCamp webpage here as reference on how to use insertAdjacentHTML() correctly
+            weatherCards.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem))
+        });
     }).catch(() => {
         alert("error fetching the weather forecast");
     })
 }
-
 
 // function to get city coordinates
 const getCityCoordinates = ()=> {
